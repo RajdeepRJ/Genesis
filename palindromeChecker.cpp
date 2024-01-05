92061abc1ac9e50345f34eb85e348e09ebf7f66e
RajdeepRJ <125926200+RajdeepRJ@users.noreply.github.com>
2024-01-05 16:11:56 +0530

    Update palindromeChecker.cpp

--- palindromeChecker.cpp
+++ palindromeChecker.cpp
@@ -2,20 +2,22 @@
 #include <string>
 #include <algorithm>
 
+using namespace std;
+
 bool isPalindrome(int num) {
-    std::string numStr = std::to_string(num);
-    std::string reversed = numStr;
-    std::reverse(reversed.begin(), reversed.end());
+    string numStr = to_string(num);
+    string reversed = numStr;
+    reverse(reversed.begin(), reversed.end());
     return numStr == reversed;
 }
 
 int main() {
     int number;
-    std::cout << "Enter a number: ";
-    std::cin >> number;
+    cout << "Enter a number: ";
+    cin >> number;
 
     bool result = isPalindrome(number);
 
-    std::cout << "The number " << number << " is a palindrome: " << std::boolalpha << result << std::endl;
+    cout << "The number " << number << " is a palindrome: " << boolalpha << result << endl;
     return 0;
 }
